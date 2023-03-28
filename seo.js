71d80b6e52c5d423ed5747e8e2a970b6623d1fb4
Clement <clement07052005@gmail.com>
2023-03-28 17:47:46 +0200

    testing with predefined meta tags

---
+++ seo.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        path: '/',
+        component: 'Home',
+        description: 'Home page',
+    },
+    {
+        path: '/test',
+        component: 'Test',
+        description: 'Test page',
+    }
+]
--- server.js
+++ server.js
@@ -4,8 +4,8 @@
   const express = require("express");
   const morgan = require("morgan");
   const path = require("path");
-
-
+  const fs = require("fs");
+  const seo = require("./seo");
   const app = express();
   app.disable("x-powered-by");
 
@@ -16,9 +16,26 @@
 
     app.use(express.static(path.join(__dirname, "./client/build")));
     //Routes
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
+app.get("*", (req, res) => {
+  let pathname = req.originalUrl || req.path;
+
+  console.log(pathname);
+  let page = seo.find((page) => page.path === pathname);
+  if (!page) {
+    page = {
+      title: "Page not found | 404",
+      description: "404",
+    };
+  }
+  let html = fs.readFileSync(
+    path.join(__dirname, "./client/build/index.html"))
+  let htmlWithMeta = html
+    .toString()
+    .replace("__SEO_TITLE__", page.title)
+    .replace("__SEO_DESCRIPTION__", page.description);
+  return res.send(htmlWithMeta);
+
+});
   
 
   app.listen(process.env.PORT || 2589, () => {
